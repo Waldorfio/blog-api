42316de4d2237b2a0bfcb4ff46977073d93161f8
Walid VM <walid.hanifi707@gmail.com>
2023-01-19 16:40:11 +1000

    Enable render of user controller functions

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -2,22 +2,22 @@ const express = require('express');
 const router = express.Router();
 const passport = require("passport");
 
-// // Require controllers
-// const userController = require('../controllers/userController');
+// Require controllers
+const userController = require('../controllers/userController');
 
-// // * USER ROUTES
-// // SHOW ALL
-// router.get('/allusers', userController.users_page); // render page of all users
-// // CREATE
-// router.get('/user/create', userController.user_create_get); // redirect to user create form
-// router.post('/user/create', userController.user_create_post);
-// // READ
-// router.get('/user/:id', userController.user_read);
-// // UPDATE
-// router.post('/user/:id', userController.user_update);
-// // DESTROY
-// router.get('/user/:id/delete', userController.user_destroy_get); // redirect to delete page, asking to confirm deletion
-// router.post('/user/:id/delete', userController.user_destroy_post); // process delete.js submit button
+// * USER ROUTES
+// SHOW ALL
+router.get('/allusers', userController.users_page); // render page of all users
+// CREATE
+router.get('/user/create', userController.user_create_get); // redirect to user create form
+router.post('/user/create', userController.user_create_post);
+// READ
+router.get('/user/:id', userController.user_read);
+// UPDATE
+router.post('/user/:id', userController.user_update);
+// DESTROY
+router.get('/user/:id/delete', userController.user_destroy_get); // redirect to delete page, asking to confirm deletion
+router.post('/user/:id/delete', userController.user_destroy_post); // process delete.js submit button
 
 // * LOGIN ROUTES
 // LOG IN
