74203e26eb0f2e5dcc4a52abe4dba65f6323017e
Walid VM <walid.hanifi707@gmail.com>
2023-01-20 17:35:07 +1000

    Simplify user schema

--- models/user.js
+++ models/user.js
@@ -3,11 +3,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    firstname: { type: String, required: true },
-    lastname: { type: String, required: true },
     username: { type: String, required: true },
     password: { type: String, required: true },
-    membership: { type: Boolean, default: false },
     admin: { type: Boolean, required: true, default: false },
 })
 
