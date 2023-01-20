52125457c342f721884f7b36af4304e87e214021
Walid VM <walid.hanifi707@gmail.com>
2023-01-20 18:04:33 +1000

    Add boilerplate async callbacks

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -4,6 +4,14 @@ const { body, validationResult } = require('express-validator');
 const async = require('async');
 const bcrypt = require('bcryptjs');
 
+const msgs_page = async (req, res, next) => {}
+const msg_create_get = async (req, res, next) => {}
+const msg_create_post = async (req, res, next) => {}
+const msg_read = async (req, res, next) => {}
+const msg_update = async (req, res, next) => {}
+const msg_destroy_get = async (req, res, next) => {}
+const msg_destroy_post = async (req, res, next) => {}
+
 module.exports = {
     msgs_page,
     msg_create_get,
--- controllers/postsController.js
+++ controllers/postsController.js
@@ -4,6 +4,14 @@ const { body, validationResult } = require('express-validator');
 const async = require('async');
 const bcrypt = require('bcryptjs');
 
+const posts_page = async (req, res, next) => {}
+const posts_create_get = async (req, res, next) => {}
+const posts_create_post = async (req, res, next) => {}
+const posts_read = async (req, res, next) => {}
+const posts_update = async (req, res, next) => {}
+const posts_destroy_get = async (req, res, next) => {}
+const posts_destroy_post = async (req, res, next) => {}
+
 module.exports = {
     posts_page,
     posts_create_get,
