ae26f14f106118c1669b9fdab2a8e3ea8fa0e1f0
Walid VM <walid.hanifi707@gmail.com>
2023-01-20 18:02:04 +1000

    Add blog post and messages routing

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -3,13 +3,45 @@ const router = express.Router();
 const passport = require("passport");
 
 // Require controllers
+const postsController = require('../controllers/postsController');
+const msgController = require('../controllers/messageController');
 const userController = require('../controllers/userController');
+
+// * BLOG POST ROUTES
+// SHOW ALL
+router.get('/allposts', postsController.posts_page); // TODO: Change route to index page for all blog posts
+// CREATE
+router.get('/post/create', postsController.posts_create_get); // redirect to create form
+router.post('/post/create', postsController.posts_create_post);
+// READ
+router.get('/post/:id', postsController.posts_read);
+// UPDATE
+router.post('/post/:id', postsController.posts_update);
+// DESTROY
+router.get('/post/:id/delete', postsController.posts_destroy_get); // redirect to delete page, asking to confirm deletion
+router.post('/post/:id/delete', postsController.posts_destroy_post); // process delete.js submit button
+
+// * POST MESSAGE ROUTES
+// TODO Fix routes below (conflicting id's maybe?)
+// SHOW ALL
+router.get('/allmsgs', msgController.msgs_page); // TODO: Change route to index page for all blog posts
+// CREATE
+router.get('/post/:id/msg/create', msgController.msg_create_get); // redirect to create form
+router.post('/post/:id/msg/create', msgController.msg_create_post);
+// READ
+router.get('/post/:id/msg/:id', msgController.msg_read);
+// UPDATE
+router.post('/post/:id/msg/:id', msgController.msg_update);
+// DESTROY
+router.get('/post/:id/msg/:id/delete', msgController.msg_destroy_get); // redirect to delete page, asking to confirm deletion
+router.post('/post/:id/msg/:id/delete', msgController.msg_destroy_post); // process delete.js submit button
+
 
 // * USER ROUTES
 // SHOW ALL
 router.get('/allusers', userController.users_page); // render page of all users
 // CREATE
-router.get('/user/create', userController.user_create_get); // redirect to user create form
+router.get('/user/create', userController.user_create_get); // redirect to create form
 router.post('/user/create', userController.user_create_post);
 // READ
 router.get('/user/:id', userController.user_read);
