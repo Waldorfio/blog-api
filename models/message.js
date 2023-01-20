1f9d8d12152e72b8cb589dd8a296ca5dacaf8cf5
Walid VM <walid.hanifi707@gmail.com>
2023-01-20 17:43:46 +1000

    Add publish status to model

--- models/message.js
+++ models/message.js
@@ -6,6 +6,7 @@ const MessageSchema = new Schema({
     username: { type: String, required: true },
     date: { type: Date, required: false, default: Date.now },
     text: { type: String, required: true },
+    padmin: { type: Boolean, required: true, default: true },
 })
 
 // Export model
