7ec46954d496825de41a8287c241cddc076f5c51
Walid VM <walid.hanifi707@gmail.com>
2023-01-21 18:18:29 +1000

    Fix typo

--- models/message.js
+++ models/message.js
@@ -6,7 +6,7 @@ const MessageSchema = new Schema({
     username: { type: String, required: true },
     date: { type: Date, required: false, default: Date.now },
     text: { type: String, required: true },
-    padmin: { type: Boolean, required: true, default: true },
+    admin: { type: Boolean, required: true, default: true },
 })
 
 // Export model
