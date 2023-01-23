6008126519080ac842fa9b40a3e0d2492cf5feb5
Wally <walid.hanifi707@gmail.com>
2023-01-23 14:33:38 +1000

    Remove reference to duplicate collection elements

--- models/message.js
+++ models/message.js
@@ -3,11 +3,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const MessageSchema = new Schema({
-    post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
-    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    date: { type: Date, required: false, default: Date.now },
+    post: { type: Schema.Types.ObjectId, ref: "Post", required: true }, // To extract the id of the associated post
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true }, // To extract the username and admin status from associated user
+    date: { type: Date, required: false, default: Date.now }, // The date of the message posted
     text: { type: String, required: true },
-    admin: { type: Boolean, required: true, default: true },
 })
 
 // Export model
