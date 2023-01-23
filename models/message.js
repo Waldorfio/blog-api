dd66a40cfabbea8708475c6d2754ac685abbb863
Wally <walid.hanifi707@gmail.com>
2023-01-23 14:31:43 +1000

    Add reference to documents in other collections

--- models/message.js
+++ models/message.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const MessageSchema = new Schema({
-    username: { type: String, required: true },
+    post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
     date: { type: Date, required: false, default: Date.now },
     text: { type: String, required: true },
     admin: { type: Boolean, required: true, default: true },
