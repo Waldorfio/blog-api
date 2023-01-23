61df0a25a59dd409e911ed6eca91cadebcef0e9f
Wally <walid.hanifi707@gmail.com>
2023-01-23 14:52:13 +1000

    Simplify HTTP request method calls

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -8,46 +8,26 @@ const msgController = require('../controllers/messageController');
 const userController = require('../controllers/userController');
 
 // * BLOG POST ROUTES
-// SHOW ALL
-router.get('/allposts', postsController.posts_page); // TODO: Change route to index page for all blog posts
-// CREATE
-router.get('/post/create', postsController.posts_create_get); // redirect to create form
-router.post('/post/create', postsController.posts_create_post);
-// READ
-router.get('/post/:id', postsController.posts_read);
-// UPDATE
-router.post('/post/:id', postsController.posts_update);
-// DESTROY
-router.get('/post/:id/delete', postsController.posts_destroy_get); // redirect to delete page, asking to confirm deletion
-router.post('/post/:id/delete', postsController.posts_destroy_post); // process delete.js submit button
+router.get('/posts', postsController.posts_page); // show all posts on the homepad
+router.post('/posts/create', postsController.posts_create_post); // CREATE
+router.get('/posts/:id', postsController.posts_read); // READ
+router.put('/posts/:id', postsController.posts_update); // UPDATE
+router.delete('/posts/:id/delete', postsController.posts_destroy_post); // DESTROY
 
 // * POST MESSAGE ROUTES
-// TODO Fix routes below (conflicting id's maybe?)
-// SHOW ALL
-router.get('/allmsgs', msgController.msgs_page); // TODO: Change route to index page for all blog posts
-// CREATE
-router.get('/post/:id/msg/create', msgController.msg_create_get); // redirect to create form
-router.post('/post/:id/msg/create', msgController.msg_create_post);
-// READ
-router.get('/post/:id/msg/:id', msgController.msg_read);
-// UPDATE
-router.post('/post/:id/msg/:id', msgController.msg_update);
-// DESTROY
-router.get('/post/:id/msg/:id/delete', msgController.msg_destroy_get); // redirect to delete page, asking to confirm deletion
-router.post('/post/:id/msg/:id/delete', msgController.msg_destroy_post); // process delete.js submit button
+router.get('/posts/:id', msgController.msgs_page); // show all messages on an individual post
+router.post('/posts/:id/msg/create', msgController.msg_create_post); // CREATE
+router.get('/posts/:id/msg/:id', msgController.msg_read); // READ
+router.put('/posts/:id/msg/:id', msgController.msg_update); // UPDATE
+router.delete('/posts/:id/msg/:id/delete', msgController.msg_destroy_post); // DESTROY
 
 
 // * USER ROUTES
-// SHOW ALL
-router.get('/allusers', userController.users_page); // render page of all users
-// CREATE
-router.post('/user/create', userController.user_create);
-// READ
-router.get('/user/:id', userController.user_read);
-// UPDATE
-router.post('/user/:id', userController.user_update);
-// DESTROY
-router.post('/user/:id/delete', userController.user_destroy); // process delete.js submit button
+router.get('/users', userController.users_page); // render page of all users
+router.post('/users/create', userController.user_create); // CREATE
+router.get('/users/:id', userController.user_read); // READ
+router.put('/users/:id', userController.user_update); // UPDATE
+router.delete('/users/:id/delete', userController.user_destroy); // DESTROY
 
 // * LOGIN ROUTES
 // LOG IN
