bf2dd9acd724f3883a7e886a409f8a246e3079f4
Walid VM <walid.hanifi707@gmail.com>
2023-01-30 13:17:24 +1000

    Update post id and username schemas

--- models/message.js
+++ models/message.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const MessageSchema = new Schema({
-    // post: { type: Schema.Types.ObjectId, ref: "Post", required: true }, // To extract the id of the associated post
-    // user: { type: Schema.Types.ObjectId, ref: "User", required: true }, // To extract the username and admin status from associated user
+    postid: { type: String, required: true }, // id of the associated post
+    username: { type: String, required: true }, // username of user
     date: { type: Date, required: false, default: Date.now }, // The date of the message posted
     text: { type: String, required: true },
 })
