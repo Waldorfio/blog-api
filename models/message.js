6b51f1c91f76fc6bef3854ae7c7bc348a3df1345
Walid VM <walid.hanifi707@gmail.com>
2023-01-30 13:05:27 +1000

    Add date to msg schema

--- models/message.js
+++ models/message.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const MessageSchema = new Schema({
     // post: { type: Schema.Types.ObjectId, ref: "Post", required: true }, // To extract the id of the associated post
     // user: { type: Schema.Types.ObjectId, ref: "User", required: true }, // To extract the username and admin status from associated user
-    // date: { type: Date, required: false, default: Date.now }, // The date of the message posted
+    date: { type: Date, required: false, default: Date.now }, // The date of the message posted
     text: { type: String, required: true },
 })
 
